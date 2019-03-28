ce20d21ef23d031606f5511db0d9ae5245b31228
Isabela Costa <43935450+isabelacostasouza@users.noreply.github.com>
2019-03-28 12:50:27 -0300

    Update Aerospace.cpp

--- Libraries/Aerospace/Aerospace.cpp
+++ Libraries/Aerospace/Aerospace.cpp
@@ -210,6 +210,145 @@ void Aerospace::accelero_calibrate()
 }
 
 
+//BME280
+
+
+float Aerospace::BME_getTemperature() {
+    int32_t var1, var2, t_fine;
+
+    bme280_calib.dig_T1 = BME_read16_LE(BME280_REGISTER_DIG_T1);
+    bme280_calib.dig_T2 = BME_readS16_LE(BME280_REGISTER_DIG_T2);
+    bme280_calib.dig_T3 = BME_readS16_LE(BME280_REGISTER_DIG_T3);
+
+    int32_t adc_T = BME_read24(BME280_REGISTER_TEMPDATA);
+    if (adc_T == 0x800000)
+        return NAN;
+    adc_T >>= 4;
+
+    var1 = ((((adc_T>>3) - ((int32_t)bme280_calib.dig_T1 <<1))) *
+            ((int32_t)bme280_calib.dig_T2)) >> 11;
+             
+    var2 = (((((adc_T>>4) - ((int32_t)bme280_calib.dig_T1)) *
+              ((adc_T>>4) - ((int32_t)bme280_calib.dig_T1))) >> 12) *
+            ((int32_t)bme280_calib.dig_T3)) >> 14;
+
+    t_fine = var1 + var2;
+
+    float T = (t_fine * 5 + 128) >> 8;
+    return T/100;
+}
+
+float Aerospace::BME_getPressure() {
+    int64_t var1, var2, p;
+
+    BME_getTemperature(); // must be done first to get t_fine
+
+    bme280_calib.dig_P1 = BME_read16_LE(BME280_REGISTER_DIG_P1);
+    bme280_calib.dig_P2 = BME_readS16_LE(BME280_REGISTER_DIG_P2);
+    bme280_calib.dig_P3 = BME_readS16_LE(BME280_REGISTER_DIG_P3);
+    bme280_calib.dig_P4 = BME_readS16_LE(BME280_REGISTER_DIG_P4);
+    bme280_calib.dig_P5 = BME_readS16_LE(BME280_REGISTER_DIG_P5);
+    bme280_calib.dig_P6 = BME_readS16_LE(BME280_REGISTER_DIG_P6);
+    bme280_calib.dig_P7 = BME_readS16_LE(BME280_REGISTER_DIG_P7);
+
+    int32_t adc_P = BME_read24(BME280_REGISTER_PRESSUREDATA);
+    if (adc_P == 0x800000) // value in case pressure measurement was disabled
+        return NAN;
+    adc_P >>= 4;
+
+    var1 = ((int64_t)t_fine) - 128000;
+    var2 = var1 * var1 * (int64_t)bme280_calib.dig_P6;
+    var2 = var2 + ((var1*(int64_t)bme280_calib.dig_P5)<<17);
+    var2 = var2 + (((int64_t)bme280_calib.dig_P4)<<35);
+    var1 = ((var1 * var1 * (int64_t)bme280_calib.dig_P3)>>8) +
+           ((var1 * (int64_t)bme280_calib.dig_P2)<<12);
+    var1 = (((((int64_t)1)<<47)+var1))*((int64_t)bme280_calib.dig_P1)>>33;
+
+    if (var1 == 0) {
+        return 0; // avoid exception caused by division by zero
+    }
+    p = 1048576 - adc_P;
+    p = (((p<<31) - var2)*3125) / var1;
+    var1 = (((int64_t)bme280_calib.dig_P9) * (p>>13) * (p>>13)) >> 25;
+    var2 = (((int64_t)bme280_calib.dig_P8) * p) >> 19;
+
+    p = ((p + var1 + var2) >> 8) + (((int64_t)bme280_calib.dig_P7)<<4);
+    return (float)p/256;
+}
+
+
+float Aerospace::BME_getHumidity() {
+
+    bme280_calib.dig_H1 = BME_read8(BME280_REGISTER_DIG_H1);
+    bme280_calib.dig_H2 = BME_readS16_LE(BME280_REGISTER_DIG_H2);
+    bme280_calib.dig_H3 = BME_read8(BME280_REGISTER_DIG_H3);
+    bme280_calib.dig_H4 = (BME_read8(BME280_REGISTER_DIG_H4) << 4) | (BME_read8(BME280_REGISTER_DIG_H4+1) & 0xF);
+    bme280_calib.dig_H5 = (BME_read8(BME280_REGISTER_DIG_H5+1) << 4) | (BME_read8(BME280_REGISTER_DIG_H5) >> 4);
+    bme280_calib.dig_H6 = (int8_t)BME_read8(BME280_REGISTER_DIG_H6);
+
+    BME_getTemperature(); // must be done first to get t_fine
+
+    int32_t adc_H = BME_read16(BME280_REGISTER_HUMIDDATA);
+    if (adc_H == 0x8000) // value in case humidity measurement was disabled
+        return NAN;
+        
+    int32_t v_x1_u32r;
+
+    v_x1_u32r = (t_fine - ((int32_t)76800));
+
+    v_x1_u32r = (((((adc_H << 14) - (((int32_t)bme280_calib.dig_H4) << 20) -
+                    (((int32_t)bme280_calib.dig_H5) * v_x1_u32r)) + ((int32_t)16384)) >> 15) *
+                 (((((((v_x1_u32r * ((int32_t)bme280_calib.dig_H6)) >> 10) *
+                      (((v_x1_u32r * ((int32_t)bme280_calib.dig_H3)) >> 11) + ((int32_t)32768))) >> 10) +
+                    ((int32_t)2097152)) * ((int32_t)bme280_calib.dig_H2) + 8192) >> 14));
+
+    v_x1_u32r = (v_x1_u32r - (((((v_x1_u32r >> 15) * (v_x1_u32r >> 15)) >> 7) *
+                               ((int32_t)bme280_calib.dig_H1)) >> 4));
+
+    v_x1_u32r = (v_x1_u32r < 0) ? 0 : v_x1_u32r;
+    v_x1_u32r = (v_x1_u32r > 419430400) ? 419430400 : v_x1_u32r;
+    float h = (v_x1_u32r>>12);
+    return  h / 1024.0;
+}
+
+bool Aerospace::BME_begin() {
+    _i2caddr = BME280_ADDRESS;
+    _wire = &Wire;
+    return init();
+}
+
+bool Aerospace::BME_init() {
+    if (_cs == -1) 
+        _wire -> BME_begin();
+    else {
+        digitalWrite(_cs, HIGH);
+        pinMode(_cs, OUTPUT);
+        if (_sck == -1)
+            SPI.begin();
+        else {
+            pinMode(_sck, OUTPUT);
+            pinMode(_mosi, OUTPUT);
+            pinMode(_miso, INPUT);
+        }
+    }
+
+    if (BME_read8(BME280_REGISTER_CHIPID) != 0x60)
+        return false;
+
+    write8(BME280_REGISTER_SOFTRESET, 0xB6);
+    delay(300);
+
+    while (isReadingCalibration())
+          delay(100);
+
+    readCoefficients(); 
+    setSampling();
+    delay(100);
+
+    return true;
+}
+
+
 
 
 
